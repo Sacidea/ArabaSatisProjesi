699f9486b7f434cb64e4209e16731b93a0633164
sacide <157601350+Sacidea@users.noreply.github.com>
2025-07-31 22:56:48 +0300

    Araç listesi  sayfası oluşturuldu

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\public\view;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
+use App\Models\CarBrand;
 
 
 class AdminController extends Controller
@@ -14,14 +15,20 @@ class AdminController extends Controller
         return view ('panel.admin.index');
     }
     public function carBrandindex(){
-        $user=auth::user();
-    
-        if($user->role==1){
-        return view('panel.admin.carBrand.index');
-        }else{
-            return abort(403);
+               //Car listesini getir
+               $brands=CarBrand::get();
+
+        //$user=auth::user();//erişim kontrolleri middlewzre de yatığımız için if bloğuna gerek kalmadı
+        //if($user->role==1){
+        return view('panel.admin.carBrand.index', compact('brands'));
+       // }else{
+        //  return abort(403);
+        
     }
-}
+
+
+    
+
     public function carBrandCreatePage(){
         return view('panel.admin.carBrand.create');
 
